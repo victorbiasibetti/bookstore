163e46a1eb63c7cf42080297bc63c54cbb9813c6
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-03-10 23:34:24 -0300

    control user logged

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,24 @@
+import { GetServerSideProps } from "next";
 import React from "react";
 import Login from "../modules/login";
 // import { Container } from './styles';
 
 const LoginPage: React.FC = () => {
   return <Login />;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const access_token = req?.cookies?.access_token;
+
+  if (access_token)
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/books",
+      },
+    };
+
+  return { props: {} };
 };
 
 export default LoginPage;
