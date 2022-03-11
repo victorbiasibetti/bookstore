db5533ef7d954a67650bd1403349fe4b8a601482
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-03-10 23:35:16 -0300

    add new book

--- src/pages/books/new.tsx
+++ src/pages/books/new.tsx
@@ -1,9 +1,22 @@
+import { GetServerSideProps } from "next";
 import React from "react";
+import { NewBook } from "../../modules/books";
 
-// import { Container } from './styles';
-
-const Book: React.FC = () => {
-  return <div />;
+const NewBooksPage: React.FC = () => {
+  return <NewBook />;
 };
 
-export default Book;
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const access_token = req?.cookies?.access_token;
+  if (!access_token)
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/?exit=1",
+      },
+    };
+
+  return { props: {} };
+};
+
+export default NewBooksPage;
