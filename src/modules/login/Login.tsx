72c871ebe0eecad47a032c8fefa95002be06ccd3
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-03-10 23:35:26 -0300

    add login function

--- src/modules/login/Login.tsx
+++ src/modules/login/Login.tsx
@@ -1,16 +1,37 @@
 import React, { useState } from "react";
+import { useRouter } from "next/router";
+import Cookies from "universal-cookie";
 
-// import { Container } from './styles';
+const cookies = new Cookies();
+
 type LoginType = {
-  email?: string;
-  password?: string;
+  email: string;
+  password: string;
 };
 
 const Login: React.FC = () => {
-  const [login, setLogin] = useState<LoginType>();
+  const router = useRouter();
+  const [login, setLogin] = useState<LoginType>({
+    email: "",
+    password: "",
+  });
 
   const handleLogin = () => {
     console.log("login", login);
+    fetch("http://localhost:3004/user")
+      .then((res) => res.json())
+      .then((result) => {
+        if (
+          result.email === login.email &&
+          result.password === login.password
+        ) {
+          console.log("success");
+          cookies.set("access_token", 1);
+          router.push("/books");
+        } else {
+          console.log("error");
+        }
+      });
   };
   return (
     <div>
@@ -18,13 +39,14 @@ const Login: React.FC = () => {
       <span>E-mail: </span>
       <input
         placeholder={"E-mail"}
-        value={login?.email}
+        value={login.email}
         onChange={(e) => setLogin({ ...login, email: e.target.value })}
       />
       <span>Password: </span>
       <input
         type={"password"}
         placeholder={"Password"}
+        value={login.password}
         onChange={(e) => setLogin({ ...login, password: e.target.value })}
       />
       <button onClick={handleLogin}>Login</button>
