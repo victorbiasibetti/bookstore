0c63eef38c3bb66362e413d59f88ea53407a6903
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-03-13 23:29:33 -0300

    update pages of book

--- src/pages/books/[id].tsx
+++ src/pages/books/[id].tsx
@@ -1,10 +1,11 @@
 import { GetServerSideProps } from "next";
 import React from "react";
-import { NewBook } from "../../modules/books";
+import { Detail } from "../../modules/books";
 import request from "../../services/request";
+import { IBooks } from "../../interfaces/books";
 
-const BooksPage: React.FC = () => {
-  return <NewBook />;
+const BooksPage: React.ReactNode = ({ book }: { book: IBooks }) => {
+  return <Detail book={book} />;
 };
 
 export const getServerSideProps: GetServerSideProps = async ({
@@ -23,9 +24,13 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   let book = {};
   try {
-    const response = await request.get(`/books?id=${id}`);
-    book = response;
-  } catch (e) {}
+    const { data }: { data: Array<IBooks> } = await request.get(
+      `/books?id=${id}`
+    );
+    if (data.length) book = data[0];
+  } catch (e) {
+    console.log(e);
+  }
 
   return { props: { book } };
 };
--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -1,10 +1,12 @@
 import { GetServerSideProps } from "next";
 import React from "react";
 import { Books } from "../../modules/books";
+import request from "../../services/request";
+import { IBooks } from "../../interfaces/books";
 // import { Container } from './styles';
 
-const BooksPage: React.FC = () => {
-  return <Books />;
+const BooksPage: React.ReactNode = ({ books }: { books: Array<IBooks> }) => {
+  return <Books books={books} />;
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
@@ -17,7 +19,9 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
       },
     };
 
-  return { props: {} };
+  const { data } = await request.get(`/books`);
+
+  return { props: { books: data } };
 };
 
 export default BooksPage;
