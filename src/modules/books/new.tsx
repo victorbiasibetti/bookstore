b2c716c2c79cb50aec8d1425f6e948dd2d089628
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-03-14 00:46:44 -0300

    add books

--- src/modules/books/new.tsx
+++ src/modules/books/new.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { IBooks } from "../../interfaces/books";
+import request from "../../services/request";
 
 // import { Container } from './styles';
 
-const NewBook: React.FC = () => {
-  return <div> New book</div>;
+const NewBook: React.ElementType = () => {
+  const router = useRouter();
+
+  const [title, setTitle] = useState<string>();
+
+  const handleSaveBook = async () => {
+    try {
+      await request.post(`/books`, {
+        title,
+      });
+      router.push(`/books`);
+    } catch (e) {}
+  };
+  return (
+    <div>
+      New Book
+      <div>
+        <label>Title</label>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      </div>
+      <button onClick={handleSaveBook}>Save</button>
+      <button onClick={() => router.push(`/books`)}>Cancel</button>
+    </div>
+  );
 };
 
 export default NewBook;
